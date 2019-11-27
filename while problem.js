0989d644500e25752661af1f8be2dc8d5d49e8d6
Rahul <rahulramesh26@yahoo.com>
2019-11-27 18:26:33 +0530

    problems using if and while

---
+++ while problem.js
@@ -0,0 +1,31 @@
+var x=-10;
+while(x<=19){
+    console.log(x);
+    x++;
+}
+var num=10;
+while(num <=40){
+    if(num%2===0){
+        console.log(num +" is even");
+    }
+    else{
+        console.log( num + " is not even")
+    }
+    num++;
+}
+var no=300;
+while(no <=333){
+    if(no%3===0){
+        console.log(no);
+    }
+    no++;
+}
+var y=5;
+while(y<=50){
+    if(y%5===0 && y%3===0){
+        console.log(y);
+    }
+    y++;
+}
+
+
